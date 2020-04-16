a90179e85f3ac00f8b8c62f88c76f2f496a77507
Dennis Geldhof <github@geldhof.eu>
2020-04-16 13:50:25 +0200

    Fix example

--- Sample/MainWindow.xaml.cs
+++ Sample/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using Denxorz.InputOutputSnappingCanvas;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -12,13 +14,32 @@ namespace Sample
            new ColorPrinter { DataContext = new ColorPrinterViewModel(529, 285) },
            new DarkLight { DataContext = new DarkLightViewModel(260, 205) },
            new AnimalPrinter { Left = 621, Top = 74 },
-           new ItemWithColor { DataContext = new ItemWithColorViewModel(429, 84, Brushes.Blue) }, 
-           new ItemWithColor { DataContext = new ItemWithColorViewModel(73, 84, Brushes.Green) },
+           new ItemWithColor { DataContext = new ItemWithColorViewModel(429, 84, Brushes.Blue) },
+           new ItemWithColor { DataContext = new ItemWithColorViewModel(125, 84, Brushes.Green) },
         };
 
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += (s, e) => FindVisualChild<InputOutputSnappingCanvas>(this).ForceLinkAll();
+        }
+
+        public TChildItem FindVisualChild<TChildItem>(DependencyObject obj) where TChildItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child != null && child is TChildItem)
+                    return (TChildItem)child;
+
+                var childOfChild = FindVisualChild<TChildItem>(child);
+
+                if (childOfChild != null)
+                    return childOfChild;
+            }
+
+            return null;
         }
     }
 }
